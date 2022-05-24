c21c29e3bce72b4c88864d15591b58efca8b7dad
KhinMaMaKyaw <kmmk26615@gmail.com>
2022-05-24 23:24:45 +0630

    design

---
+++ cart.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <div>
+        <table>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Total</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Carrot Juice</td>
+                <td><img src="./images/carrot.jpg"></td>
+                <td>1500 MMK</td>
+                <td>1</td>
+                <td>1500MMK</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Apple Juice</td>
+                <td><img src="./images/apple.jpg"></td>
+                <td>1500 MMK</td>
+                <td>2</td>
+                <td>3000 MMK</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <h1>Available Juice</h1>
+    <div class="container">
+        <div class="card">
+            <div class="img">
+                <img src="./images/avocado.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Acocado Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/apple.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Apple Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/carrot.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Carrot Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/lemon.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Lemon Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/mango.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Mango Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/mixfruit.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Mixfruit Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/orange.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Orange Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/pineapple.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Pineapple Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/strawberry.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Strawberry Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./images/watermelon.jpg" alt="photo">
+            </div>
+            <div class="product_name">
+                <p>Watermelon Juice</p>
+            </div>
+            <div class="price">
+                <p>1500 MMK</p>
+            </div>
+            <div>
+                <input type="submit" value="BY NOW">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
